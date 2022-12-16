9327fbb3516f7554c9a56f9a8d7bf0053bdb2593
tianxinyue611 <104822349+tianxinyue611@users.noreply.github.com>
2022-12-15 20:34:17 -0500

    Update robot_control.py

--- robot_control.py
+++ robot_control.py
@@ -19,26 +19,22 @@ import time
 
 
 def distance():
-    # 发送高电平信号到 Trig 引脚
     GPIO.output(GPIO_TRIGGER, True)
 
-    # 持续 10 us
     time.sleep(0.00001)
     GPIO.output(GPIO_TRIGGER, False)
 
     start_time = time.time()
     stop_time = time.time()
 
-    # 记录发送超声波的时刻1
     while GPIO.input(GPIO_ECHO) == 0:
         start_time = time.time()
 
-    # 记录接收到返回超声波的时刻2
     while GPIO.input(GPIO_ECHO) == 1:
         stop_time = time.time()
-    # 计算超声波的往返时间 = 时刻2 - 时刻1
+
     time_elapsed = stop_time - start_time
-    # 声波的速度为 343m/s， 转化为 34300cm/s。
+
     distance = (time_elapsed * 34300) / 2
 
     return distance
